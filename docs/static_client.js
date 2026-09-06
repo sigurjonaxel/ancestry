cdf32e4598250e34eb23cd3ffe056afe04397434
sigurjonaxel <sigurjonaxel@users.noreply.github.com>
2026-09-06 10:29:46 +0000

    Bust cache: Force fetch static_data.json with timestamp

--- docs/static_client.js
+++ docs/static_client.js
@@ -6,7 +6,7 @@ let staticDataPromise = null;
 
 function getStaticData() {
   if (!staticDataPromise) {
-    staticDataPromise = fetch('static_data.json')
+    staticDataPromise = fetch('static_data.json?v=' + Date.now())
       .then(res => res.json())
       .then(data => {
         STATIC_DATA = data;
